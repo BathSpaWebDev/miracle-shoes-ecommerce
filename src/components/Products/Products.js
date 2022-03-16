41bfd0dedbe7824af77771fd4dc5c73f05c1c18f
Andrei Btc <andreibtc@gmail.com>
2022-03-16 14:38:17 +0000

    Adds product card styling and card components

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,11 +3,13 @@ import ShoeData from "../../shoes.json";
 
 export const Products = () => {
   return (
-    <div>
-      {ShoeData.results.map((shoe, i) => (
-        <div key={i}>
+    <div className="products_container">
+      {ShoeData.results.map((shoe) => (
+        <div key={shoe.id} className="product_card">
           <img src={shoe.urls.thumb} alt="shoe pic" />
-          <p>{shoe.description}</p>
+          <h2>{shoe.productName}</h2>
+          <p>{shoe.alt_description}</p>
+          <h3>Price £100</h3>
         </div>
       ))}
     </div>
