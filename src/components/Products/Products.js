7d63ce85a0d51f91846124f0c739953f7dd0133a
Andrei Btc <andreibtc@gmail.com>
2022-03-16 12:23:33 +0000

    Maps Json data into Products Page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,5 +1,15 @@
 import "./Products.css";
+import ShoeData from "../../shoes.json";
 
 export const Products = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      {ShoeData.results.map((shoe, i) => (
+        <div key={i}>
+          <img src={shoe.urls.thumb} alt="shoe pic" />
+          <p>{shoe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
