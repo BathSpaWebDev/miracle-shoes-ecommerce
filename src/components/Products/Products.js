1449ad18d3f6faeedd608aac901d417478a5ac78
Andrei Btc <andreibtc@gmail.com>
2022-03-16 16:38:36 +0000

    NOthing

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -5,11 +5,14 @@ export const Products = () => {
   return (
     <div className="products_container">
       {ShoeData.results.map((shoe) => (
-        <div key={shoe.id} className="product_card">
-          <img src={shoe.urls.thumb} alt="shoe pic" />
-          <h2>{shoe.productName}</h2>
-          <p>{shoe.alt_description}</p>
-          <h3>Price £100</h3>
+        <div>
+          <div key={shoe.id} className="product_card">
+            <img src={shoe.urls.thumb} alt="shoe pic" />
+            <h2>{shoe.productName}</h2>
+            <p>{shoe.alt_description}</p>
+            <h3>Price £100</h3>
+          </div>
+          <a href={shoe.user.portfolio_url}>{shoe.user.name}</a>
         </div>
       ))}
     </div>
